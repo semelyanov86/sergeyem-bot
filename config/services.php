3de88b68653b922d2c134cf6a21eb41f5c4989e5
Sergey Emelyanov <se@sergeyem.ru>
2025-04-27 19:33:36 +0200

    feature: add check command

--- config/services.php
+++ config/services.php
@@ -53,4 +53,18 @@ return [
         'server' => env('FIREFLY_SERVER', 'https://finance.sergeyem.ru/api/v1'),
         'token' => env('FIREFLY_TOKEN'),
     ],
+    'checker' => [
+        'websites' => [
+            'https://sergeyem.ru' => 'WEB-разработчик, внедрение CRM. Программист Laravel',
+            'https://sergeyem.eu' => 'Ein erfahrener Webentwickler',
+            'https://cloud.sergeyem.ru/index.php/login' => '<a href="https://owncloud.com" target="_blank" rel="noreferrer">ownCloud</a>',
+            'https://keys.sergeyem.ru:8443/#/login' => '<i class="bwi bwi-spinner bwi-spin bwi-3x tw-text-muted" title="Loading" aria-hidden="true"></i>',
+            'https://itvolga.com' => '+7(8352)22-36-06',
+            'https://creditcoop.ru' => 'Кредитная кооперация Чувашии: кредитные кооперативы и союзы',
+            'https://mautic.sergeyem.ru/s/login' => 'keep me logged in',
+            'https://mautic.itvolga.com/s/login' => 'keep me logged in',
+            'https://links.sergeyem.ru/login' => 'Forgot your password?',
+            'https://easywordsapp.ru/#/login' => '<link rel="stylesheet" href="https://easywordsapp.ru/build/assets',
+        ],
+    ],
 ];
