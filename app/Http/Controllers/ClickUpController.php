1da060cf62b6c29f50004c526314912dfcb398a3
Sergey Emelyanov <35343883+semelyanov86@users.noreply.github.com>
2026-02-23 10:22:14 +0100

    Update ClickUpController.php

--- app/Http/Controllers/ClickUpController.php
+++ app/Http/Controllers/ClickUpController.php
@@ -6,11 +6,11 @@ namespace App\Http\Controllers;
 
 use App\Services\ClickUpWebhookService;
 use Illuminate\Http\Request;
-use Illuminate\Http\Response;
+use Illuminate\Http\JsonResponse;
 
 final class ClickUpController extends Controller
 {
-    public function webhook(Request $request, ClickUpWebhookService $service): Response
+    public function webhook(Request $request, ClickUpWebhookService $service): JsonResponse
     {
         if (! config('services.clickup.webhook_enabled')) {
             abort(403, 'ClickUp webhooks are disabled');
